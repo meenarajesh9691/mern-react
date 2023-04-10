72bd7835af649500d1295ac771add4edfbde9d9f
RAJESH <rajesh.meena1010@gmail.com>
2023-04-10 20:15:02 +0530

    Two Way Binding

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,48 @@
 
 import React, { useState } from 'react'
+import { nanoid } from 'nanoid'
 
 const index = () => {
   //javascript logic
-  const [Time, setTime] = useState(new Date().toLocaleTimeString());
+  const [Title, setTitle] = useState("")
+  const [Desc, setDesc] = useState("")
 
-  setInterval(() => {
-    setTime(new Date().toLocaleTimeString());
-  }, 1000);
-
+  
+  const CreateTaskHandler = (e)=>{
+    e.preventDefault();
+    const todo ={
+      id:nanoid(),
+      Title:Title,
+      Description:Desc
+    };
+    console.log(todo);
+  };
+  //  let chnageHandler = (e)=> setTitle(e.target.value);
 
   return (
     <>
-      <h2>{Time}</h2>
+      <div className="container bg-light w-50 mt-5 p-5">
+        <h1 className='mb-4'>Todo-App</h1>
+
+        <form action="" onSubmit={CreateTaskHandler}>
+          <input className='form-control mb-4' type="text" placeholder='Title'  
+             onChange={(e) => setTitle(e.target.value)}
+             value={Title}
+          />
+
+
+          <input className='form-control' type="text" placeholder='Description' onChange={(e) => setDesc(e.target.value)} 
+          value={Desc}
+          />
+          
+          <button className='btn btn-primary mt-3'>Create Task</button>
+        </form>
+        <hr />
+        <ul>
+          <h3 className='text-danger fw-light'>Pending Tasks</h3>
+          <li className='d-flex justify-content-between fs-3'>title 1<i className="text-danger ri-delete-bin-2-line"></i></li>
+        </ul>
+      </div>
     </>
   )
 }
