06eb9b2991ff2d08de47f042d513563e7105415c
RAJESH <rajesh.meena1010@gmail.com>
2023-04-10 18:15:25 +0530

    Live Time

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,16 @@ import React, { useState } from 'react'
 
 const index = () => {
   //javascript logic
-  let [myName, setName] = useState("Rajesh");
+  const [Time, setTime] = useState(new Date().toLocaleTimeString());
 
-  let changeName = () => {
-    setName("Rajesh meena");
-    console.log("Done click button");
-  };
+  setInterval(() => {
+    setTime(new Date().toLocaleTimeString());
+  }, 1000);
+
 
   return (
     <>
-      <h1>{myName}</h1>
-      <button onClick={changeName}>Change Name</button>
+      <h2>{Time}</h2>
     </>
   )
 }
