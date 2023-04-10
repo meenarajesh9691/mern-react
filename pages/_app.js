52ba54d29fe0d40e40a1c5fe72d4b31ecc599c23
RAJESH <rajesh.meena1010@gmail.com>
2023-04-10 16:39:14 +0530

    useState

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '@/styles/globals.css'
+import "@/node_modules/bootstrap/dist/css/bootstrap.min.css";
+import 'remixicon/fonts/remixicon.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
